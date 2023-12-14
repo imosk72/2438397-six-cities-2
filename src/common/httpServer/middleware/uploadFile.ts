92eca6abe963f41b942095cf908347544432b4dc
Ivan Moskovchenko <imosk72@yandex-team.ru>
2023-12-14 20:41:44 +0300

    added upload file middleware

---
+++ src/common/httpServer/middleware/uploadFile.ts
@@ -0,0 +1,32 @@
+import { nanoid } from 'nanoid';
+import multer, { diskStorage } from 'multer';
+import { extension } from 'mime-types';
+import { NextFunction, Request, Response } from 'express';
+import { IMiddleware } from './IMiddleware.js';
+
+export class UploadFileMiddleware implements IMiddleware {
+  private readonly uploadDirectory: string;
+  private readonly fieldName: string;
+
+  constructor(
+    uploadDirectory: string,
+    fieldName: string,
+  ) {
+    this.uploadDirectory = uploadDirectory;
+    this.fieldName = fieldName;
+  }
+
+  public async execute(request: Request, response: Response, next: NextFunction): Promise<void> {
+    const storage = diskStorage({
+      destination: this.uploadDirectory,
+      filename: (_req, file, callback) => {
+        const fileExtension = extension(file.mimetype);
+        const filename = nanoid();
+        callback(null, `${filename}.${fileExtension}`);
+      },
+    });
+
+    const uploadSingleFileMiddleware = multer({ storage }).single(this.fieldName);
+    uploadSingleFileMiddleware(request, response, next);
+  }
+}
